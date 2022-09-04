b1011a5070632941821684f6fc98595b444d9fe7
Sahil Samar <sahilsamar031@gmail.com>
2022-09-04 11:34:04 -0700

    Added obstacle to game and jump

--- JavaCode/Controller.java
+++ JavaCode/Controller.java
@@ -2,46 +2,89 @@ package JavaCode;
 
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
+import java.awt.Rectangle;
+
 
 public class Controller {
 	int x = 0;
-    int y = 330;
-	int xa = 0;
-    int ya = 0;
+    int y = 300;
+	int vx = 0;
+    int vy = 1;
+	private static final int WIDTH = 30;
+	private static final int HEIGHT = 30;
 	private Game game;
+
+	Obstacle obstacle = new Obstacle(game);
+
 
 	public Controller(Game game) {
 		this.game= game;
 	}
 
 	public void move() {
-		if (x + xa > 0 && x + xa < game.getWidth()-60)
-			x = x + xa;
-        if (y + ya > 0 && x + ya < game.getHeight()-60)
-            y = y + ya;
+		if (x + vx > 0 && x + vx < game.getWidth()-60)
+			x = x + vx;
+        if (y + vy > 0 && x + vy < game.getHeight()-60)
+            y = y + vy;
+		if (collisionObstacle()) {
+			game.gameOver();
+		}
+		if (y < game.getHeight() / 2) {
+			if (collisionGround()) {
+				vy = -1;
+			}
+		}
+		else {
+			if (collisionGround()) {
+				vy = 0;
+			}
+			else {
+				vy = 1;
+			}
+
+		}
+
 	}
 
 	public void paint(Graphics2D g) {
-		g.fillOval(x, y, 30, 30);
+		g.fillOval(x, y, WIDTH, HEIGHT);
+		obstacle.paint(g, 100);
+
+	}
+
+	public Rectangle getBounds() {
+		return new Rectangle(x, y, WIDTH, HEIGHT);
 	}
 
 	public void keyReleased(KeyEvent e) {
-		xa = 0;
-		while (! (329 < y) && (331 >y)) {
-			ya = 2;
-			y += ya;
-		}
-		ya = 0;
-
-
+		vx = 0;
 	}
 
 	public void keyPressed(KeyEvent e) {
 		if (e.getKeyCode() == KeyEvent.VK_A)
-			xa = -1;
+			vx = -2;
 		if (e.getKeyCode() == KeyEvent.VK_D)
-			xa = 1;
-        if (e.getKeyCode() == KeyEvent.VK_SPACE)
-            ya = -1;
+			vx = 2;
+		if (vy == 0) {
+			if (e.getKeyCode() == KeyEvent.VK_SPACE)
+				do {
+					y = y - 1;
+				} 
+				while (y > 250);
+				vy = -1;
+		}
+
 	}
+
+	private boolean collisionGround() {
+		return game.ground.getBounds().intersects(getBounds());
+	}
+
+	private boolean collisionObstacle() {
+		return this.obstacle.getBounds().intersects(getBounds());
+	}
+
+	
+
+
 }
--- JavaCode/game.java
+++ JavaCode/game.java
@@ -7,13 +7,21 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JOptionPane;
+
 
 @SuppressWarnings("serial")
 public class Game extends JPanel {
 
+	// Instantiating the player (ball)
 	Controller controller = new Controller(this);
+	// Instantiating the ground
+	Ground ground = new Ground(this);
+
 
 	public Game() {
+		// Adding key listener to constructor. 
+		// This allows for user input.
 		addKeyListener(new KeyListener() {
 			@Override
 			public void keyTyped(KeyEvent e) {
@@ -32,30 +40,45 @@ public class Game extends JPanel {
 		setFocusable(true);
 	}
 	
+	// Game loop, simply calling the method for the player to move.
 	private void move() {
 		controller.move();
 	}
 
+	// Overriding the paint method.
+	// Super.paint is needed since we repaint every 10 miliseconds.
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 				RenderingHints.VALUE_ANTIALIAS_ON);
+		// Painting the ground and the player.
 		controller.paint(g2d);
+		ground.paint(g2d);
 	}
 
+	// Simple Game Over screen
+	public void gameOver() {
+		JOptionPane.showMessageDialog(this, "Game Over", "Game Over", JOptionPane.YES_NO_OPTION);
+		System.exit(ABORT);
+	}
+
+	// Creating the JFrame
 	public static void main(String[] args) throws InterruptedException {
-		JFrame frame = new JFrame("Mini Tennis");
+		JFrame frame = new JFrame("ADfastpages Game");
+		// New Game Object
 		Game game = new Game();
 		frame.add(game);
 		frame.setSize(300, 400);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		// The game loop. Causes the progression of things in the game.
 		while (true) {
 			game.move();
 			game.repaint();
+			// Things aren't updated every frame. They are updated every 10 ms.
 			Thread.sleep(10);
 		}
 	}
